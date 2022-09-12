1647e70e51217a7f72319ca089da547f0151e357
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-09-12 19:56:44 +0300

    Добавьте файлы проекта.

---
+++ SafeDevelopment_Lesson5/CertificateConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeDevelopment_Lesson5
+{
+    public class CertificateConfiguration
+    {
+        public X509Certificate2 RootCertificate { get; set; }
+        public int CertDuration { get; set; }
+        public string CertName { get; set; }
+        public string Password { get; set; }
+        public string OutFolder { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ SafeDevelopment_Lesson5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeDevelopment_Lesson5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("~~ Центр генерации сертификатов ~~~\n");
+                Console.WriteLine("1. Создать корневой сертификат");
+                Console.WriteLine("2. Создать сертификат");
+                Console.Write("Выберите подпрограмму (0 - завершение работы приложения): ");
+                if (int.TryParse(Console.ReadLine(), out int no))
+                {
+                    switch (no)
+                    {
+                        case 0:
+                            Console.WriteLine("Завершение работы приложения.");
+                            Console.ReadKey();
+                            return;
+                        case 1:                            
+                            break;
+                        case 2:                            
+                            break;
+                        default:
+                            Console.WriteLine("Некорректный номер подпрограммы. Пожалуйста, повторите ввод.");
+                            break;
+                    }
+                }
+                else
+                    Console.WriteLine("Некорректный номер подпрограммы. Пожалуйста, повторите ввод.");
+            }
+        }
+    }
+}
